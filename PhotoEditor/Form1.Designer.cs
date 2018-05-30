4fe02ef8ef803a9ec739c0e1f045d71dadabb7af
kusmp <patryk.kusmierkiewicz@gmail.com>
2018-05-30 23:29:34 +0200

    first version

--- PhotoEditor/Form1.Designer.cs
+++ PhotoEditor/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,18 +37,23 @@
             this.eNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oAplikacjiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_SelectPenColor = new System.Windows.Forms.Button();
+            this.cmb_SelectBrushSize = new System.Windows.Forms.ComboBox();
+            this.btn_DrawSquare = new System.Windows.Forms.Button();
+            this.btn_DrawRectangle = new System.Windows.Forms.Button();
+            this.btn_DrawCircle = new System.Windows.Forms.Button();
+            this.txt_SelectShapeSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(229, 31);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(525, 371);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // menuStrip1
             // 
@@ -58,7 +62,7 @@
             this.oAplikacjiToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(766, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(980, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -121,27 +125,164 @@
             this.closeToolStripMenuItem.Text = "Wyjdź";
             this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(152, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(816, 505);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btn_SelectPenColor
+            // 
+            this.btn_SelectPenColor.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btn_SelectPenColor.Location = new System.Drawing.Point(31, 60);
+            this.btn_SelectPenColor.Name = "btn_SelectPenColor";
+            this.btn_SelectPenColor.Size = new System.Drawing.Size(75, 23);
+            this.btn_SelectPenColor.TabIndex = 3;
+            this.btn_SelectPenColor.UseVisualStyleBackColor = false;
+            this.btn_SelectPenColor.Click += new System.EventHandler(this.btn_SelectPenColor_Click);
+            // 
+            // cmb_SelectBrushSize
+            // 
+            this.cmb_SelectBrushSize.FormattingEnabled = true;
+            this.cmb_SelectBrushSize.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.cmb_SelectBrushSize.Location = new System.Drawing.Point(31, 20);
+            this.cmb_SelectBrushSize.Name = "cmb_SelectBrushSize";
+            this.cmb_SelectBrushSize.Size = new System.Drawing.Size(75, 21);
+            this.cmb_SelectBrushSize.TabIndex = 4;
+            // 
+            // btn_DrawSquare
+            // 
+            this.btn_DrawSquare.Location = new System.Drawing.Point(33, 26);
+            this.btn_DrawSquare.Name = "btn_DrawSquare";
+            this.btn_DrawSquare.Size = new System.Drawing.Size(75, 23);
+            this.btn_DrawSquare.TabIndex = 5;
+            this.btn_DrawSquare.Text = "Kwadrat";
+            this.btn_DrawSquare.UseVisualStyleBackColor = true;
+            this.btn_DrawSquare.Click += new System.EventHandler(this.btn_DrawSquare_Click);
+            // 
+            // btn_DrawRectangle
+            // 
+            this.btn_DrawRectangle.Location = new System.Drawing.Point(33, 55);
+            this.btn_DrawRectangle.Name = "btn_DrawRectangle";
+            this.btn_DrawRectangle.Size = new System.Drawing.Size(75, 23);
+            this.btn_DrawRectangle.TabIndex = 6;
+            this.btn_DrawRectangle.Text = "Prostokąt";
+            this.btn_DrawRectangle.UseVisualStyleBackColor = true;
+            this.btn_DrawRectangle.Click += new System.EventHandler(this.btn_DrawRectangle_Click);
+            // 
+            // btn_DrawCircle
+            // 
+            this.btn_DrawCircle.Location = new System.Drawing.Point(33, 84);
+            this.btn_DrawCircle.Name = "btn_DrawCircle";
+            this.btn_DrawCircle.Size = new System.Drawing.Size(75, 23);
+            this.btn_DrawCircle.TabIndex = 7;
+            this.btn_DrawCircle.Text = "Okrąg";
+            this.btn_DrawCircle.UseVisualStyleBackColor = true;
+            this.btn_DrawCircle.Click += new System.EventHandler(this.btn_DrawCircle_Click);
+            // 
+            // txt_SelectShapeSize
+            // 
+            this.txt_SelectShapeSize.Location = new System.Drawing.Point(19, 130);
+            this.txt_SelectShapeSize.Name = "txt_SelectShapeSize";
+            this.txt_SelectShapeSize.Size = new System.Drawing.Size(100, 20);
+            this.txt_SelectShapeSize.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Rozmiar pędzla";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Kolor pędzla";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Kształt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Rozmiar kształtu";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btn_SelectPenColor);
+            this.panel2.Controls.Add(this.cmb_SelectBrushSize);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(9, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(137, 92);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.btn_DrawSquare);
+            this.panel3.Controls.Add(this.txt_SelectShapeSize);
+            this.panel3.Controls.Add(this.btn_DrawCircle);
+            this.panel3.Controls.Add(this.btn_DrawRectangle);
+            this.panel3.Location = new System.Drawing.Point(9, 125);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(137, 155);
+            this.panel3.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 414);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(980, 544);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oAplikacjiToolStripMenuItem;
@@ -151,6 +292,19 @@
         private System.Windows.Forms.ToolStripMenuItem pLToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem eNToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_SelectPenColor;
+        private System.Windows.Forms.ComboBox cmb_SelectBrushSize;
+        private System.Windows.Forms.Button btn_DrawSquare;
+        private System.Windows.Forms.Button btn_DrawRectangle;
+        private System.Windows.Forms.Button btn_DrawCircle;
+        private System.Windows.Forms.TextBox txt_SelectShapeSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- PhotoEditor/Form1.cs
+++ PhotoEditor/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,27 @@ namespace PhotoEditor
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        bool startPaint = false;
+        int? initX = null;
+        int? initY = null;
+        bool drawSquare = false;
+        bool drawRectangle = false;
+        bool drawCircle = false;
+        List<Rectangle> rectangles = new List<Rectangle>();
+        Rectangle mRect = Rectangle.Empty;
+
         public Form1()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
+            cmb_SelectBrushSize.SelectedIndex = 0;
+            panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
         }
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -29,8 +48,9 @@ namespace PhotoEditor
 
             if (ofd.ShowDialog() == DialogResult.OK && ofd.FileName.Length > 0)
             {
-                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-                pictureBox1.Image = Image.FromFile(ofd.FileName);
+                panel1.AutoSize = true;
+                panel1.BackgroundImageLayout = ImageLayout.Zoom;
+                panel1.BackgroundImage = Image.FromFile(ofd.FileName);
             }
         }
 
@@ -41,13 +61,108 @@ namespace PhotoEditor
 
             if (sfd.ShowDialog() == DialogResult.OK && sfd.FileName.Length > 0)
             {
-                pictureBox1.Image.Save(sfd.FileName);
+                using (Bitmap graphicSurface = new Bitmap(panel1.Width, panel1.Height))
+                {
+                    using (StreamWriter bitmapWriter = new StreamWriter(sfd.FileName))
+                    {
+                        panel1.DrawToBitmap(graphicSurface, new Rectangle(0, 0, panel1.Width, panel1.Height));
+                        graphicSurface.Save(bitmapWriter.BaseStream, ImageFormat.Jpeg);
+                    }
+                }
             }
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (startPaint)
+            {
+                Pen p = new Pen(btn_SelectPenColor.BackColor, float.Parse(cmb_SelectBrushSize.Text));
+                g.DrawLine(p, new Point(initX ?? e.X, initY ?? e.Y), new Point(e.X, e.Y));
+                initX = e.X;
+                initY = e.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPaint = true;
+            if (drawSquare)
+            {
+               
+                SolidBrush sb = new SolidBrush(btn_SelectPenColor.BackColor);
+          
+                g.FillRectangle(sb, e.X, e.Y, int.Parse(txt_SelectShapeSize.Text), int.Parse(txt_SelectShapeSize.Text));
+              
+                startPaint = false;
+                drawSquare = false;
+            }
+            if (drawRectangle)
+            {
+                SolidBrush sb = new SolidBrush(btn_SelectPenColor.BackColor);
+                g.FillRectangle(sb, e.X, e.Y, 2 * int.Parse(txt_SelectShapeSize.Text), int.Parse(txt_SelectShapeSize.Text));
+                startPaint = false;
+                drawRectangle = false;
+            }
+            if (drawCircle)
+            {
+                SolidBrush sb = new SolidBrush(btn_SelectPenColor.BackColor);
+                g.FillEllipse(sb, e.X, e.Y, int.Parse(txt_SelectShapeSize.Text), int.Parse(txt_SelectShapeSize.Text));
+                startPaint = false;
+                drawCircle = false;
+            }
+        }
+
+        private void btn_SelectPenColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                btn_SelectPenColor.BackColor = color.Color;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            startPaint = false;
+            initX = null;
+            initY = null;
+        }
+
+        private void btn_DrawSquare_Click(object sender, EventArgs e)
+        {
+            drawSquare = true;
+        
+        }
+
+        private void btn_DrawRectangle_Click(object sender, EventArgs e)
+        {
+            drawRectangle = true;
+        }
+
+        private void btn_DrawCircle_Click(object sender, EventArgs e)
+        {
+            drawCircle = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+  
+         
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        
         }
     }
 }
